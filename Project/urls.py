2bd4a108e53746cfe930765cf5a005d1053b9ac3
umasankarmg03-netizen <umasankarmg03@gmail.com>
2026-05-24 09:40:34 +0530

    Add files via upload

---
+++ Project/urls.py
@@ -0,0 +1,52 @@
+"""
+URL configuration for Project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+
+from django.urls import path, include, re_path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth import views as auth_views
+from App.views import CustomLoginView, ChangePasswordView
+
+from App.forms import LoginForm
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('App.urls')),
+    path('accounts/', include('App.urls')),
+    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
+                                           authentication_form=LoginForm), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+#     path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
+
+    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
+
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
